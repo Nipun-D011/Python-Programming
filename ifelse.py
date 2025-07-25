ff9541531cab74ebf06653b3114dc89c0f292318
Nipun <nipundhananjaya2021.com>
2025-07-25 22:45:17 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,5 @@
+n = 11
+if n % 2 == 0 :
+    print("n is an even number")
+else:
+    print("n is an odd number")
---
+++ test3.py
@@ -0,0 +1,13 @@
+a = 5
+b = 1
+c = 1
+
+if a > b :
+    if a > c :
+        print("a value is big")
+    else :
+        print ("c value is big")
+elif b > c :
+    print("b value is big")
+else :
+    print ("c value is big")  
